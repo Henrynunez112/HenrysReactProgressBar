91cfa370f7b2b0ba473e8b19963339101ce1a47e
Henrynunez112 <henrynunez@pursuit.org>
2020-12-01 08:04:08 -0500

    working on vanilla css on the app

--- henry_progress_bar/src/Component/ProgressBar/ProgressBar.js
+++ henry_progress_bar/src/Component/ProgressBar/ProgressBar.js
@@ -1,37 +1,38 @@
 import React from 'react';
+import "./ProgressBar.css";
 
-const ProgressBar = ({bgColor, completed}) =>{
-    const containerStyles = {
-        height: 20,
-        width: '100%',
-        backgroundColor: "#e0e0de",
-        borderRadius: 50,
-        margin: 50
-      }
-    
-      const fillerStyles = {
-        height: '100%',
-        width: `${completed}%`,
-        backgroundColor: bgColor,
-        borderRadius: 'inherit',
-        textAlign: 'right',
-        transition: 'width 1s ease-in-out'
-      }
-    
-      const labelStyles = {
-        padding: 5,
-        color: 'white',
-        fontWeight: 'bold'
-      }
+const ProgressBar = ({ bgColor, completed }) => {
+  const containerStyles = {
+    height: 20,
+    width: '100%',
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    // margin: 50
+  }
 
-    return(
-        <div style={containerStyles}>
-            <div style={fillerStyles}>
-    <span style={labelStyles}>{`${completed}`}</span>
-            </div>
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgColor,
+    borderRadius: 'inherit',
+    textAlign: 'right',
+    transition: 'width 1s ease-in-out'
+  }
 
-        </div>
-    )
+  const labelStyles = {
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+
+  return (
+    <div style={containerStyles} className="containerStyles">
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}`}</span>
+      </div>
+
+    </div>
+  )
 
 }
 export default ProgressBar;
